822509a363c7600eaf2ae8852be60cb66f99dec9
James Wu <jameswlb01@gmail.com>
2024-11-13 21:17:19 +0800

    Add first draft of sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let difference = num1 - num2;
+    let sum = 0;
+    if (difference > 0){
+        // Case 1: num1 is greater than num2
+        for (let i = 1; i <= difference+1; i++){
+            sum += num2;
+            num2++;
+        }
+    } else if (difference < 0){
+        // Case 2: num2 is greater than num2
+        for (let i = 1; i <= difference+1; i++){
+        sum += num1;
+        num1++;
+    } else {
+        sum = num1 + num2;
+    }
+    }
+    return sum;
 };
 
 // Do not edit below this line
