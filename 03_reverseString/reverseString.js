98926614cc50747ed8530499b28359543a05306d
James Wu <jameswlb01@gmail.com>
2024-11-13 14:51:40 +0800

    Update the reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(strings) {
+    let len = strings.length;
+    let reverse_string ='';
+    for (let i = len - 1; i>=0 ; i--) {
+        reverse_string += strings[i];
+    }
+    return reverse_string;
 };
 
 // Do not edit below this line
