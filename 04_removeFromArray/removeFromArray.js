af7e86e17ac68b19d96f2e9fb5c10c15abdce980
James Wu <jameswlb01@gmail.com>
2024-11-13 15:28:25 +0800

    Update removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, word) {
+    let new_array = [];
+    for (let i = 0; i < length(num); i++) {
+        if (word != array[i]) {
+            new_array.push(array(i));
+        }
+    }
+    return new_array;
 };
 
 // Do not edit below this line
