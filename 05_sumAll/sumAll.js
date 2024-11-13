e419c585acd176f5d99a710035c2f01b2c0522fd
James Wu <jameswlb01@gmail.com>
2024-11-13 21:26:27 +0800

    Fix Case 2 to accomondate for all cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -9,14 +9,14 @@ const sumAll = function(num1, num2) {
         }
     } else if (difference < 0){
         // Case 2: num2 is greater than num2
-        for (let i = 1; i <= difference+1; i++){
+        for (let i = -1; i >= difference-1; i--){
         sum += num1;
         num1++;
+        } 
     } else {
         sum = num1 + num2;
-    }
-    }
-    return sum;
+        }
+        return sum;
 };
 
 // Do not edit below this line
