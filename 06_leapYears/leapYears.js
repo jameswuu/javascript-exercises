a5a386a247078134e2ab940c57e7acb864f277c9
James Wu <jameswlb01@gmail.com>
2024-11-14 09:51:11 +0800

    Add codes to check for leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    // Leap years are years divisible by 4
+    // Also year disvisble by 100 are not unlesss they can be divided by 400
+    if (year%4 === 0 && year%100 !== 0) {
+        return true;
+    } else if (year%100 === 0 && year%400 === 0) {
+        return false;
+    }
+    
 
 };
 
