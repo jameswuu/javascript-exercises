2dbc343c064d63d6b51dd1e3c0c6287efda23615
James Wu <jameswlb01@gmail.com>
2024-11-14 10:01:48 +0800

    Add codes for temperature conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5.0 / 9.0;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9.0 / 5.0 + 32);
+  return fahrenheit;
 };
 
 // Do not edit below this line
