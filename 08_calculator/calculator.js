3f0e931e1e8dbaa85861e89138b9b00a0c8c33b9
James Wu <jameswlb01@gmail.com>
2024-11-17 10:56:26 +0800

    Complete all functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return (num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  // old ways to do it
+  // let sum = 0;
+  // for(let i = 0; i < array.length; i++) {
+  //   sum += array[i];
+  // }
+  // return sum;
+
+  // Cleaner way of doing it using reduce
+  const sum = array.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0) 
+  return sum;
+
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  // Using reduce to perform product
+  const productOfAllNums = array.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 
+  return productOfAllNums;
 };
 
-const power = function() {
-	
+const power = function(base, root) {
+  let answer =  1;
+
+  for (let i = 0; i < root; i++) {
+    answer *= base;
+  }
+
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let answer = 1;
+  for (let i = 1; i < (num + 1); i++) {
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
