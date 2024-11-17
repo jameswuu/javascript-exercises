5d4a471dab9c0ae7c568b77235e7b27ba52112c3
James Wu <jameswlb01@gmail.com>
2024-11-17 11:37:51 +0800

    Add palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Since we only consider letters and numbers, create a variable containing all valid characters
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    // Convert to lowercase, split to array of individual characters, filter only valid characters, then rejoin as new string
+    const cleanedString = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    // Reverse the filterered_array
+    let reverse_filtered_array = cleanedString.split('').reverse().join('');
+
+    // Assume palindromes is true
+    let Ispalindromes = true;
+
+    // Check if the reversed and the original one are identical
+    if (reverse_filtered_array !== cleanedString){
+        Ispalindromes = false;
+    }
+
+    return Ispalindromes;
 };
 
 // Do not edit below this line
