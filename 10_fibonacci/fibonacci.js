77dcec66498ee351623f7cb0f988be4490ece5ae
James Wu <jameswlb01@gmail.com>
2024-11-17 21:52:50 +0800

    Add fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num >= 0) {
+        // Initialize the beginning of the function
+        let array = [0,1];
+
+        // Find the fibonacci for the current num
+        for (let i = 1; i < (num + 1); i++) {
+            let num1 = array[i-1]; // The first number 
+            let num2 = array[i]; // The second number
+
+            let currentNumber = num1 + num2; // number to the array
+            array.push(currentNumber);
+        }
+
+        // Return the answer
+        return(array[num]);
+    } 
 
 };
 
