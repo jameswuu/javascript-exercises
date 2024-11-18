bccca6141bfe37b0d0ba43610938fb3701e1bead
James Wu <jameswlb01@gmail.com>
2024-11-18 08:27:15 +0800

    Create for loop to go through object to find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // Inialize varables
+    let currentYear = date();
+    let oldestPerson = '';
+    let oldestAge = null;
 
+    // Use loop to go throught each person
+    for (let person of people){
+        let currentAge;
+        if (person.yearofDeath) {
+            currentAge = person.yearofDeath - yearofBirth
+        } else {
+            currentAge = currentYear - yearOfBirth;
+        }
+    
+        // Compare the oldest one with the current one
+        if (currentAge >= oldestAge || oldestAge != null) {
+            oldestPerson = person.name;
+        } else {
+            oldestAge = currentAge;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
